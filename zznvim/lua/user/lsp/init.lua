5b7ca7f8c318ab304dbea1b601c9ccd747fd37aa
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-01-25 21:58:46 +1000

    adding old vim config

---
+++ zznvim/lua/user/lsp/init.lua
@@ -0,0 +1,10 @@
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+require "user.lsp.lsp-installer"
+require("user.lsp.handlers").setup()
+require "user.lsp.null-ls"
